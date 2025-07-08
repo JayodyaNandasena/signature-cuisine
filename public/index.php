cd47ca8498820ee33b380f70c95a893d2744e99f
Jayodya Nandasena <jayodyanandasena@gmail.com>
2025-07-08 16:17:50 +0530

    Add index.php for application entry point with setup check

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = require_once '../assets/config/config.php';
+
+// Check if setup is needed
+if (! file_exists($config['paths']['setup_flag'])) {
+    // Direct to set up page
+    header('Location: ../setup/setup.php');
+    exit;
+}
+
+// Normal application flow
+header('Location: ../controllers/home.php');
+exit;
