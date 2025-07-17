2a6b3727e89876603e49f3969846412a228f2402
Jayodya Nandasena <jayodyanandasena@gmail.com>
2025-07-17 14:29:47 +0530

    Refactor menu display to dynamically fetch and render menu items; adjust background positioning for better layout

--- views/partials/menu.php
+++ views/partials/menu.php
@@ -1,3 +1,32 @@
+<?php
+
+require_once __DIR__ . '/../../Core/Database.php';
+
+use Core\database;
+
+try {
+    // Get database instance
+    $db = database::getInstance();
+
+    // Fetch main menu items
+    $mainsMenu = $db->query("SELECT name, price FROM food WHERE category = 'main' ORDER BY id ASC LIMIT 5")->get();
+
+    // Fetch dessert menu items
+    $dessertsMenu = $db->query("SELECT name, price FROM food WHERE category = 'dessert' ORDER BY id ASC LIMIT 5")->get();
+
+    // Fetch beverage menu items
+    $beveragesMenu = $db->query("SELECT name, price FROM food WHERE category = 'beverage' ORDER BY id ASC LIMIT 5")->get();
+
+} catch (Exception $e) {
+    // Handle errors
+    error_log("database error: " . $e->getMessage());
+    $mainsMenu = [];
+    $dessertsMenu = [];
+    $beveragesMenu = [];
+    $error = "Unable to fetch menu items. Please try again later.";
+}
+?>
+
 <section id="menu" class="ourPackage slider_overlay">
     <div class="container">
         <div class="main_package_content">
@@ -7,25 +36,23 @@
                     <h3>Savor the Art of Flavor</h3>
                 </div>
 
-                <div class="single_package_one text-right wow rotateInDownRight">
-                    <div class="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4">
+                <div class="single_package_one text-left wow rotateInDownRight">
+                    <div class="col-md-6 col-sm-8">
                         <div class="single_package_text">
                             <div class="package_title">
                                 <h4>Main Courses</h4>
                             </div>
 
                             <ul>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
+                                <?php foreach ($mainsMenu as $mainItem): ?>
+                                    <li class="menu-item">
+                                        <span><?= htmlspecialchars($mainItem['name']) ?></span>
+                                        <span class="dots"></span>
+                                        <span>Rs.<?= number_format($mainItem['price'], 2) ?></span>
+                                    </li>
+                                <?php endforeach; ?>
                             </ul>
+
                         </div>
                     </div>
                 </div>
@@ -38,22 +65,19 @@
                             </div>
 
                             <ul>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
+                                <?php foreach ($dessertsMenu as $dessertItem): ?>
+                                    <li class="menu-item">
+                                        <span><?= htmlspecialchars($dessertItem['name']) ?></span>
+                                        <span class="dots"></span>
+                                        <span>Rs.<?= number_format($dessertItem['price'], 2) ?></span>
+                                    </li>
+                                <?php endforeach; ?>
                             </ul>
                         </div>
                     </div>
                 </div>
 
-                <div class="single_package_three text-left wow rotateInDownRight">
+                <div class="single_package_three text-right wow rotateInDownRight">
                     <div class="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4">
                         <div class="single_package_text">
                             <div class="package_title">
@@ -61,16 +85,13 @@
                             </div>
 
                             <ul>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
-                                <li> Tuna Roast Source........................................................$24.5
-                                </li>
+                                <?php foreach ($beveragesMenu as $beverageItem): ?>
+                                    <li class="menu-item">
+                                        <span><?= htmlspecialchars($beverageItem['name']) ?></span>
+                                        <span class="dots"></span>
+                                        <span>Rs.<?= number_format($beverageItem['price'], 2) ?></span>
+                                    </li>
+                                <?php endforeach; ?>
                             </ul>
                         </div>
                     </div>
